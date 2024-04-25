9ccbe4a37df61c1c3661d486b9927a436c2bf8d7
AmirHosseinZg <zakeri863@gmail.com>
2024-04-25 12:10:36 +0330

    In User and Book class : the variables with int data type , was changed into string data type . also some methods were added  for casting int var into string var

--- Answers/LMS_40230112063/src/main/java/basic/classes/Book.java
+++ Answers/LMS_40230112063/src/main/java/basic/classes/Book.java
@@ -1,12 +1,11 @@
 package basic.classes;
 
 class Book {
-    protected int book_id;
-    protected String title, author, description;
+    protected String title, author, description, book_id;
     protected boolean availability_status;
 
-    public Book(int book_id, boolean availability_status) {
-        this.book_id = book_id;
+    public Book(Integer book_id, boolean availability_status) {
+        this.book_id = book_id.toString();
         this.availability_status = availability_status;
     }
 }
--- Answers/LMS_40230112063/src/main/java/basic/classes/User.java
+++ Answers/LMS_40230112063/src/main/java/basic/classes/User.java
@@ -2,7 +2,13 @@ package basic.classes;
 
 class User {
 
-    protected String name;
-    protected int id, phone_number;
+    protected String name, id, phone_number;
 
+    public void castingId(Integer id){
+        this.id = id.toString();
+    }
+
+    public void castingPhoneNumber(Integer phone_number) {
+        this.id = phone_number.toString();
+    }
 }
