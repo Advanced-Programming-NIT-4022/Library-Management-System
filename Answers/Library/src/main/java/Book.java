1e2388fafb9e95d4f4f6157e49ddbeae4062f810
arshiammz <mohammadzade.arshia84@gmail.com>
2024-04-27 23:59:32 +0330

    Class Book created(1)

---
+++ Answers/Library/src/main/java/Book.java
@@ -0,0 +1,53 @@
+import java.util.UUID;
+
+public class Book {
+    private String bookID;
+    private String title;
+    private String author;
+    private boolean availability;
+    private String description;
+
+    public Book(String title, String author, String description) {
+        this.bookID = UUID.randomUUID().toString();
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.availability = true;
+    }
+    
+    public String getBookID() {
+        return bookID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
