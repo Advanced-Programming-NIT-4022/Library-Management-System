1542c88d608ee4ecdc95fbeda923d7fe683f0c1f
Anndraj <amirrezasolhjo2004@gmail.com>
2024-04-29 23:31:03 +0330

    Book class created

---
+++ Answers/LMS/src/test/java/Book.java
@@ -0,0 +1,39 @@
+public class Book {
+    private int bookID;
+    private String Title;
+    private String Author;
+    private boolean Availability;
+    private String Description;
+
+    public Book(String Title, String Author, String Description) {
+        this.Title = Title;
+        this.Author = Author;
+        this.Description = Description;
+        this.Availability = true;
+        this.bookID = bookID;
+    }
+
+    public int getBookID() {
+
+        return bookID;
+    }
+    public String getTitle(){
+
+        return Title;
+    }
+    public String getAuthor(){
+        return  Author;
+    }
+    public boolean isAvailable(){
+
+        return Availability;
+    }
+    public void setAvailable(boolean Availability){
+
+        this.Availability = Availability;
+    }
+    public String getDescription(){
+
+        return Description;
+    }
+}
