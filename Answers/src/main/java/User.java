02eb6772c08a1b4fef840a7519da4147e4d52a09
yekta <yekta.rjd@gmail.com>
2024-04-30 15:01:33 +0330

    Class user made

---
+++ Answers/src/main/java/User.java
@@ -0,0 +1,11 @@
+public class User {
+    String Name;
+    int Unique_ID;
+    int Phone_number;
+
+    public User(String Name , int Unique_ID , int Phone_number){
+        this.Name = Name;
+        this.Unique_ID = Unique_ID;
+        this.Phone_number = Phone_number;
+    }
+}
