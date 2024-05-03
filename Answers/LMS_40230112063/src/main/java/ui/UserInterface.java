b3da2cff2edd83846bada32b15033ddfb2f7499b
AmirHosseinZg <zakeri863@gmail.com>
2024-05-03 13:49:42 +0330

    UserInterface Class : RegEx was made for LMS and PortalPass CommandLine

--- Answers/LMS_40230112063/src/main/java/ui/UserInterface.java
+++ Answers/LMS_40230112063/src/main/java/ui/UserInterface.java
@@ -1,56 +1,63 @@
 package ui;
 
 import basic.classes.Library;
-
 import java.util.Scanner;
 import java.util.Objects;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserInterface {
-    String command;
-    String operation;
-    private final String super_admin_password = "AmirHosseinZg1383";
 
+    Library library = new Library("AmirZg", 500);
     Scanner input = new Scanner(System.in);
 
-    Library library = new Library("fucking library", 500);
+    private final String super_admin_password = "AmirHosseinZg1383";
+
 
     public void printLibrarySupportedCommand() {
         System.out.println("lib add book <title> <author> <description>"); //add a new book to the library
         System.out.println("lib get hrs");//retrieve library operating hours
-        System.out.println("lib rent <bookName>");//rent a book from the library
-        System.out.println("lib add member <studentID> <password>");//add new member to the library(Admin privilege required)
-        System.out.println("lib rent <bookName> <memberName> <memberID>");//rent a book for a specific member
+        System.out.println("lib rent <bookName> <author> <user_name> <phone_number>");//rent a book for a specific member
         System.out.println("lib get available books");//view available books for rental
-        System.out.println("lib remove member <memberID>");//remove a member from the library(admin privilege required)
-        System.out.println("lib return <bookName>");//return a rented book to the library
+        System.out.println("lib remove member <user-name>");//remove a member from the library(admin privilege required)
+        System.out.println("lib return <bookName> <author>");//return a rented book to the library
+        System.out.println("lib PortalPass");
+        System.out.println("*** Be sure to use the <> operator to send arguments in the command line ***");
     }
 
 
-    public void printLobbySupportedCommand() {
+    public void printPortalPassSupportedCommand() {
         System.out.println("sign in as normal user <name> <phone-number>");
         System.out.println("sign in as admin <name> <phone-number> <password>");
         System.out.println("creating new normal-user account <name> <phone-number>");
         System.out.println("add new super doer ( admin ) <name> <phone-number> <password> " +
                 "(attention :the password must have at least 8 characters)"); //admin access required
+        System.out.println("*** Be sure to use the <> operator to send arguments in the command line ***");
     }
 
 
-    public void libraryLobby() {
+    private String normal_user_name, admin_name, admin_password;
+    private Integer normal_user_phone_number, admin_phone_number;
+
+    private final String portalpass_regex1 = "^sign\\s+in\\s+as\\s+normal\\s+user\\s+<([a-zA-Z]+(?:\\s[a-zA-Z]+)*)>\\s+<((9[0-9]{9})|(09[0-9]{9}))>$";
+    private final String portalpass_regex2 = "^sign\\s+in\\s+as\\s+admin\\s+<([a-zA-Z]+(?:\\s[a-zA-Z]+)*)>\\s+<((9[0-9]{9})|(09[0-9]{9}))>\\s+<([^ ]{8,})>$";
+    private final String portalpass_regex3 = "^creating\\s+new\\s+normal\\s+user\\s+account\\s+<([a-zA-Z]+(?:\\s[a-zA-Z]+)*)>\\s+<((9[0-9]{9})|(09[0-9]{9}))>$";
+    private final String portalpass_regex4 = "^add\\s+new\\s+super\\s+doer\\s+<([a-zA-Z]+(?:\\s[a-zA-Z]+)*)>\\s+<((9[0-9]{9})|(09[0-9]{9}))>\\s+<([^ ]{8,})>$";
+
+    private final String lms_command_regex1 = "^lib\\s+add\\s+book\\s+<([0-9]*\\s*[a-zA-Z]+(?:[0-9]*\\s*[a-zA-Z]*)*)>\\s+<([a-zA-Z]+(?:\\s[a-zA-Z]+)*)>\\s+<([0-9]*\\s*[a-zA-Z]+(?:[0-9]*\\s*[a-zA-Z]*)*)>$";
+    private final String Lms_command_regex2 = "^lib\\s+get\\s+hrs$";
+    private final String lms_command_regex3 = "^lib\\s+rent\\s+<([0-9]*\\s*[a-zA-Z]+(?:[0-9]*\\s*[a-zA-Z]*)*)>\\s+<([a-zA-Z]+(?:\\s[a-zA-Z]+)*)>\\s+<([a-zA-Z]+(?:\\s[a-zA-Z]+)*)>\\s+<((9[0-9]{9})|(09[0-9]{9}))>$";
+    private final String lms_command_regex4 = "^lib\\s+get\\s+available\\s+books$";
+    private final String Lms_command_regex5 = "^lib\\s+remove\\s+member\\s+<([a-zA-Z]+(?:\\s[a-zA-Z]+)*)>$";
+    private final String Lms_command_regex6 = "^lib\\s+return\\s+<([0-9]*\\s*[a-zA-Z]+(?:[0-9]*\\s*[a-zA-Z]*)*)>\\s+<([a-zA-Z]+(?:\\s[a-zA-Z]+)*)>$";
+    private final String lms_command_regex7 = "^lib\\s+PortalPass$";
+
+    public void library() {
 
         // PortalPass
         // Sign in : logging in with existing account, Sign up : creating new account
 
         System.out.println("Welcome to library PortalPass");
-        
-        String normal_user_name, admin_name, admin_password;
-        Integer normal_user_phone_number, admin_phone_number;
-
-        String regex1 = "sign in as normal user ([a-zA-Z]+(?:\\s[a-zA-Z]+)*) ((9[0-9]{9})|(09[0-9]{9}))";
-        String regex2 = "sign in as admin ([a-zA-Z]+(?:\\s[a-zA-Z]+)*) ((9[0-9]{9})|(09[0-9]{9})) ([^ ]{8,})";
-        String regex3 = "creating new normal-user account ([a-zA-Z]+(?:\\s[a-zA-Z]+)*) ((9[0-9]{9})|(09[0-9]{9}))";
-        String regex4 = "add new super doer ( admin ) ([a-zA-Z]+(?:\\s[a-zA-Z]+)*) ((9[0-9]{9})|(09[0-9]{9})) ([^ ]{8,})";
+        String operation;
 
         while (true) {
             System.out.print("Enter your desired command : (command line is case sensitive) ");
@@ -59,28 +66,28 @@ public class UserInterface {
             System.out.print(">>");
             operation = input.nextLine();
 
-            if(Pattern.compile(regex1).matcher(operation).find()){
-                normal_user_name = Pattern.compile(regex1).matcher(operation).group(1);
-                normal_user_phone_number = Integer.parseInt(Pattern.compile(regex1).matcher(operation).group(2));
+            if(Pattern.compile(portalpass_regex1,Pattern.CASE_INSENSITIVE).matcher(operation).find()){
+                normal_user_name = Pattern.compile(portalpass_regex1).matcher(operation).group(1);
+                normal_user_phone_number = Integer.parseInt(Pattern.compile(portalpass_regex1).matcher(operation).group(2));
                 break;
             }
-            else if(Pattern.compile(regex2).matcher(operation).find()){
-                admin_name = Pattern.compile(regex2).matcher(operation).group(1);
-                admin_phone_number = Integer.parseInt(Pattern.compile(regex2).matcher(operation).group(2));
-                admin_password = Pattern.compile(regex2).matcher(operation).group(3);
+            else if(Pattern.compile(portalpass_regex2,Pattern.CASE_INSENSITIVE).matcher(operation).find()){
+                admin_name = Pattern.compile(portalpass_regex2).matcher(operation).group(1);
+                admin_phone_number = Integer.parseInt(Pattern.compile(portalpass_regex2).matcher(operation).group(2));
+                admin_password = Pattern.compile(portalpass_regex2).matcher(operation).group(3);
                 break;
             }
-            else if(Pattern.compile(regex3).matcher(operation).find()){
-                normal_user_name = Pattern.compile(regex1).matcher(operation).group(1);
-                normal_user_phone_number = Integer.parseInt(Pattern.compile(regex1).matcher(operation).group(2));
+            else if(Pattern.compile(portalpass_regex3,Pattern.CASE_INSENSITIVE).matcher(operation).find()){
+                normal_user_name = Pattern.compile(portalpass_regex1).matcher(operation).group(1);
+                normal_user_phone_number = Integer.parseInt(Pattern.compile(portalpass_regex1).matcher(operation).group(2));
                 break;
             }
-            else if(Pattern.compile(regex4).matcher(operation).find()){
+            else if(Pattern.compile(portalpass_regex4,Pattern.CASE_INSENSITIVE).matcher(operation).find()){
                 System.out.println("adding new admin for library management require super admin password , enter password : \n>>");
                 if(Objects.equals(input.nextLine(), super_admin_password)){
-                    admin_name = Pattern.compile(regex2).matcher(operation).group(1);
-                    admin_phone_number = Integer.parseInt(Pattern.compile(regex2).matcher(operation).group(2));
-                    admin_password = Pattern.compile(regex2).matcher(operation).group(3);
+                    admin_name = Pattern.compile(portalpass_regex2).matcher(operation).group(1);
+                    admin_phone_number = Integer.parseInt(Pattern.compile(portalpass_regex2).matcher(operation).group(2));
+                    admin_password = Pattern.compile(portalpass_regex2).matcher(operation).group(3);
                     break;
                 }
                 else{
@@ -95,8 +102,10 @@ public class UserInterface {
             }
         }
 
+        // Library System
 
-
+        String command;
+        System.out.println("Welcome to "+library.getLibraryName()+" Library Management System");
 
     }
 }
