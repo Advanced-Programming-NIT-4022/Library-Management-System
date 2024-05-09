56634b7ecd9633b639747cad2f9608090aab2d84
Hossein <hossein.mofidimohajjerr@gmail.com>
2024-05-10 03:21:47 +0330

    create class NormalUser

--- Answers/src/NormalUser.java
+++ Answers/src/NormalUser.java
@@ -1,23 +1,21 @@
-// this class inherits from class User
-// and simulates the NormalUser
-import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.LocalDate;
 
 public class NormalUser extends User {
-    private LocalDate registerDate;
-    private LocalTime registerTime;
+        private final LocalDate registerDate;
+        private final LocalTime registerTime;
 
-    public NormalUser(String name, String phoneNumber) {
-        super(name, phoneNumber);
-        this.registerDate = LocalDate.now();
-        this.registerTime = LocalTime.now();
-    }
+        public NormalUser(String name, String phoneNumber) throws IllegalArgumentException {
+            super(name, phoneNumber);
+            this.registerDate = LocalDate.now();
+            this.registerTime = LocalTime.now();
+        }
 
-    public LocalDate getRegisterDate() {
-        return registerDate;
-    }
+        public LocalDate getRegisterDate() {
+            return registerDate;
+        }
 
-    public LocalTime getRegisterTime() {
-        return registerTime;
+        public LocalTime getRegisterTime() {
+            return registerTime;
+        }
     }
-}
