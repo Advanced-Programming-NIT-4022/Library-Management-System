464f7bd9bff5ebee67c18df59f6ae1f4e13fb57e
Hossein <hossein.mofidimohajjerr@gmail.com>
2024-05-10 03:20:07 +0330

    delete UUID and add validate phoneNumber

--- Answers/src/User.java
+++ Answers/src/User.java
@@ -4,16 +4,19 @@ import java.util.UUID;
 
 public class User {
     private String name;
-    // unique id is based on the name of the user
-    private String uniqueID;
+    private String uniqueID; // unique id automatically creates in database
     private String phoneNumber;
 
     // constructor
+    public User(String name, String phoneNumber) throws IllegalArgumentException {
+        // validate phoneNumber
+        String regexPhoneNumber = "(\\+989|09|00989)(0[0-5]|1[0-9]|2[0-3]|3[0-9]|9[0-6]-?\\d{3}-?\\d{4})|" +
+                "(991[0134]{5})|(99[09]{6})|(998{6})";
+        if (phoneNumber != null && !phoneNumber.matches(regexPhoneNumber))
+            throw new IllegalArgumentException("Invalid phone number!");
 
-    public User(String name, String phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
-        this.uniqueID= UUID.nameUUIDFromBytes(phoneNumber.getBytes()).toString();
     }
 
     public String getName() {
@@ -22,9 +25,6 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
-
-        // the unique id should change
-        uniqueID = UUID.nameUUIDFromBytes(this.phoneNumber.getBytes()).toString();
     }
 
     public String getPhoneNumber() {
