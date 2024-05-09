f448c71d556cf1162800a21890c670b63be555b7
Amir <salehamir708@gmail.com>
2024-05-09 21:49:49 +0330

    Library class is added to project

---
+++ Answers/40230112081/Library.java
@@ -0,0 +1,12 @@
+public class Library {
+    private String libName;
+    private int libCap;
+    private String oprHours;
+
+    // repos & registries
+    myFileCLass usersFileHandle;
+    myFileCLass adminsFileHandle;
+    myFileCLass booksFileHandle;
+
+    // verifications
+}
