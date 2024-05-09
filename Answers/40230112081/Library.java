7884202bbd00864ec5f84288fc4d553822ac76e6
Amir <salehamir708@gmail.com>
2024-05-10 01:30:07 +0330

    Constructor is added to library class

--- Answers/40230112081/Library.java
+++ Answers/40230112081/Library.java
@@ -4,9 +4,34 @@ public class Library {
     private String oprHours;
 
     // repos & registries
-    myFileCLass usersFileHandle;
-    myFileCLass adminsFileHandle;
-    myFileCLass booksFileHandle;
-
+    public myFileCLass usersFileHandle;
+    public myFileCLass adminsFileHandle;
+    public myFileCLass booksFileHandle;
+    public myFileCLass rentalFileHandle;
     // verifications
+    public Verifications v;
+    // constructor
+    public Library(String name, String duration, int cap){
+        this.oprHours = duration;
+        this.libCap = cap;
+        this.libName = name;
+        usersFileHandle = new myFileCLass();
+        adminsFileHandle = new myFileCLass();
+        booksFileHandle = new myFileCLass();
+        rentalFileHandle = new myFileCLass();
+        usersFileHandle.dir = "Users.txt";
+        adminsFileHandle.dir = "Admins.txt";
+        booksFileHandle.dir = "Books.txt";
+        rentalFileHandle.dir = "Rental.txt";
+    }
+    // getters
+    public String getLibName() { return this.libName; }
+    public int getLibCap() { return this.libCap; }
+    public String getOprHours() { return this.oprHours; }
+
+    // login as admin
+
+    // login as normal user
+
+
 }
