175010145163ccbab853395998897f863624569c
Zeinab <darya.abbasi1383@gmail.com>
2024-05-09 14:18:42 +0330

    Library class completed

--- Answers/40230112089/Library/src/Library.java
+++ Answers/40230112089/Library/src/Library.java
@@ -6,5 +6,22 @@ public class Library {
     ArrayList<User> userArray = new ArrayList<>();
     ArrayList<Book> bookArray = new ArrayList<>();
     ArrayList<Library> libArray = new ArrayList<>();
-
+    ArrayList<Rent> rentArray = new ArrayList<>();
+    int usersId = 0;
+    int booksId = 0;
+    int rentsId = 0;
+    public Library(String libraryName, String workingHours, String libraryPassword) {
+        this.libraryName = libraryName;
+        this.workingHours = workingHours;
+        this.libraryPassword = libraryPassword;
+        if (!this.bookArray.isEmpty()) {
+            booksId = this.bookArray.size() + 1;
+        }
+        if (!this.userArray.isEmpty()) {
+            usersId = this.userArray.size() + 1;
+        }
+        if (!this.rentArray.isEmpty()) {
+            rentsId = this.rentArray.size() + 1;
+        }
+    }
 }
