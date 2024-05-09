48624a34c62b94c50a08a6fc05891d5d1e1ab73d
Hossein <hossein.mofidimohajjerr@gmail.com>
2024-05-10 03:25:43 +0330

    add validate password

--- Answers/src/Admin.java
+++ Answers/src/Admin.java
@@ -1,14 +1,21 @@
-public class Admin extends User {
-    private String password;
 
-    public Admin(String name, String phoneNumber, String password) {
+public class Admin extends User {
+    private final String password;
+
+    public Admin(String name, String phoneNumber, String password) throws IllegalArgumentException {
         super(name, phoneNumber);
+
+        // validate password
+        if (!password.matches(".*\\d.*") && !password.matches(".*[a-z].*") &&
+                !password.matches(".*[A-Z].*"))
+            throw new IllegalArgumentException("Password should contain at least one digit," +
+                    "one capital and small letter.");
+
         this.password = password;
     }
+
     // to verify the password
-    public boolean Verify(String password) {
-        if (this.password.equals(password))
-            return true;
-        return false;
+    public boolean verify(String password) {
+        return this.password.equals(password);
     }
 }
