29b12fb62e4f825ff5ef38da1e5dc6bd8068a19f
Zeinab <darya.abbasi1383@gmail.com>
2024-05-09 19:33:24 +0330

    isLibraryOpen & showAllCommand & checkCommand & run methods added

--- Answers/40230112089/Library/src/Library.java
+++ Answers/40230112089/Library/src/Library.java
@@ -1,18 +1,26 @@
 import java.io.*;
+import java.net.SocketTimeoutException;
+import java.sql.SQLOutput;
 import java.util.*;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 public class Library {
     String libraryName;
     String libraryPassword;
-    String workingHours;
+    int openHour;
+    int closeHour;
+
     ArrayList<User> userArray = new ArrayList<>();
     ArrayList<Book> bookArray = new ArrayList<>();
     ArrayList<Rent> rentArray = new ArrayList<>();
     int usersId = 1;
     int booksId = 1;
     int rentsId = 1;
-    public Library(String libraryName, String workingHours, String libraryPassword) {
+
+    public Library(String libraryName, int openHour, int closeHour, String libraryPassword) {
         this.libraryName = libraryName;
-        this.workingHours = workingHours;
+        this.openHour = openHour;
+        this.closeHour = closeHour;
         this.libraryPassword = libraryPassword;
         checkFiles();
         fileUpdater();
@@ -26,6 +34,7 @@ public class Library {
             rentsId = this.rentArray.size() + 1;
         }
     }
+
     public void checkFiles() {
         try {
             File file1 = new File("rents.txt");
@@ -37,6 +46,7 @@ public class Library {
         } catch (Exception ignored) {
         }
     }
+
     public void fileSaver() {
         try {
             //کلاس FileOutputStream یک جریان خروجی برای نوشتن داده‌ها به یک فایل فراهم می‌کند.
@@ -60,6 +70,7 @@ public class Library {
         } catch (Exception ignored) {
         }
     }
+
     public void fileUpdater() {
         try {
             //داده ها را از فایل باینری میخوانیم
@@ -83,6 +94,7 @@ public class Library {
         } catch (Exception ignored) {
         }
     }
+
     public void showAllUser() {
         if (!checkPassword()) {
             System.out.println("Wrong Password ");
@@ -103,6 +115,7 @@ public class Library {
             System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
         }
     }
+
     public void showAllBook() {
         if (this.bookArray.isEmpty()) {
             System.out.println("Book list is empty");
@@ -118,6 +131,7 @@ public class Library {
             System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
         }
     }
+
     public void showAllRent() {
         if (this.rentArray.isEmpty()) {
             System.out.println("Rent list is empty ");
@@ -129,7 +143,7 @@ public class Library {
         }
         System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
         for (int i = 0; i < this.rentArray.size(); i++) {
-            System.out.println(  (i + 1) + "-");
+            System.out.println((i + 1) + "-");
             System.out.println("Rent id      : " + this.rentArray.get(i).rentId);
             System.out.println("Rent date    : " + this.rentArray.get(i).rentDate);
             System.out.println("Rent book id : " + this.rentArray.get(i).book.bookId);
@@ -137,6 +151,7 @@ public class Library {
             System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
         }
     }
+
     public boolean checkUserId(int userId) {
         boolean result = false;
         for (int i = 0; i < this.userArray.size(); i++) {
@@ -153,12 +168,14 @@ public class Library {
         String password = input.nextLine();
         return libraryPassword.equals(password);
     }
+
     public boolean userCheckPassword(int userid) {
         Scanner input = new Scanner(System.in);
         System.out.print("enter User password: ");
         String userPassword = input.nextLine();
         return this.userArray.get(userid - 1).password.equals(userPassword);
     }
+
     public boolean checkBookId(int bookId) {
         boolean result = false;
         for (int i = 0; i < this.bookArray.size(); i++) {
@@ -169,6 +186,7 @@ public class Library {
         }
         return result;
     }
+
     public void addBook(String bookName, String bookAuthor) {
         if (!checkPassword()) {
             System.out.println("Wrong Password");
@@ -181,6 +199,7 @@ public class Library {
         System.out.println(" Book added");
         fileSaver();
     }
+
     public void removeBook(int bookId) {
         if (!checkBookId(bookId)) {
             System.out.println("Wrong Book");
@@ -199,6 +218,7 @@ public class Library {
         System.out.println(" Book removed");
         fileSaver();
     }
+
     public void addUser(String userName, String phoneNumber, String password) {
         if (!checkPassword()) {
             System.out.println("Wrong Password");
@@ -211,6 +231,7 @@ public class Library {
         System.out.println("User added ");
         fileSaver();
     }
+
     public void removeUser(int userId) {
         if (!checkUserId(userId)) {
             System.out.println("Wrong User");
@@ -229,6 +250,7 @@ public class Library {
         System.out.println("User removed");
         fileSaver();
     }
+
     public void rentBook(int userId, int bookId) {
         if (!checkBookId(bookId)) {
             System.out.println("Wrong Book");
@@ -265,6 +287,7 @@ public class Library {
         System.out.println(" Book rented ");
         fileSaver();
     }
+
     public void returnBook(int userId, int bookId) {
         if (!checkUserId(userId)) {
             System.out.println("Wrong user");
@@ -289,4 +312,109 @@ public class Library {
         System.out.println("Book returned");
         fileSaver();
     }
-}
+
+    public boolean isLibraryOpen() {
+        String timeZone = "Asia/Tehran";
+        LocalDateTime currentTime = LocalDateTime.now(ZoneId.of(timeZone));
+        int hour = currentTime.getHour();
+        if (hour >= openHour && hour < closeHour) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public void showAllCommand() {
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println("Book options : ");
+        System.out.println("lib add book <bookName> <bookAuthor>");
+        System.out.println("lib remove book <bookID>");
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println("User options : ");
+        System.out.println("lib add user <userName> <phoneNumber> <password>");
+        System.out.println("lib remove user <userID>");
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println("Show : ");
+        System.out.println("lib show all book");
+        System.out.println("lib show all user");
+        System.out.println("lib show all rent");
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        System.out.println("Rent & Return : ");
+        System.out.println("lib rent book <userID> <bookID> ");
+        System.out.println("lib return book <userID> <bookID>");
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+    }
+
+    public void checkCommand(String command) {
+        if (command.equals("exit")) {
+            System.exit(0);
+        } else {
+            String[] commandSegs = command.split(" ");
+            if (commandSegs[0].equals("lib")) {
+                if (commandSegs[1].equals("-h") && commandSegs.length == 2) {
+                    showAllCommand();
+                } else if (commandSegs[1].equals("add")) {
+                    if (commandSegs.length == 5 && commandSegs[2].equals("book") && commandSegs[3].matches("[a-zA-Z]+") && commandSegs[4].matches("[a-zA-Z]+")) {
+                        addBook(commandSegs[3], commandSegs[4]);
+                    } else if (commandSegs.length == 6 && commandSegs[2].equals("user") && commandSegs[3].matches("[a-zA-Z]+") && commandSegs[4].matches("[0-9]+")) {
+                        addUser(commandSegs[3], commandSegs[4], commandSegs[5]);
+                    } else {
+                        System.out.println("Wrong try agin");
+                    }
+                } else if (commandSegs[1].equals("remove")) {
+                    if (commandSegs.length == 4 && commandSegs[2].equals("user") && commandSegs[3].matches("[0-9]+")) {
+                        removeUser(Integer.parseInt(commandSegs[3]));
+
+                    } else if (commandSegs.length == 4 && commandSegs[2].equals("book") && commandSegs[3].matches("[0-9]+")) {
+                        removeBook(Integer.parseInt(commandSegs[3]));
+
+                    } else {
+                        System.out.println("Wrong try agin");
+                    }
+                } else if (commandSegs[1].equals("show") && commandSegs[2].equals("all") && commandSegs.length == 4) {
+                    if (commandSegs[3].equals("book")) {
+                        showAllBook();
+
+                    } else if (commandSegs[3].equals("user")) {
+                        showAllUser();
+                    } else if (commandSegs[3].equals("rent")) {
+                        showAllRent();
+                    } else {
+                        System.out.println("Wrong try agin");
+                    }
+
+                } else if (commandSegs[1].equals("rent") && commandSegs[2].equals("book") && commandSegs[3].matches("[0-9]+") && commandSegs[4].matches("[0-9]+") && commandSegs.length == 5) {
+                    rentBook(Integer.parseInt(commandSegs[3]), Integer.parseInt(commandSegs[4]));
+                }
+
+
+                else if (commandSegs[1].equals("return") && commandSegs[2].equals("book") && commandSegs[3].matches("[0-9]+") && commandSegs[4].matches("[0-9]+") && commandSegs.length == 5) {
+                    returnBook(Integer.parseInt(commandSegs[3]), Integer.parseInt(commandSegs[4]));
+                } else {
+                    System.out.println("Wrong try agin");
+                    return;
+                }
+            }
+
+        }
+    }
+
+
+        public void run() {
+            System.out.println("xxx Welcome to " + this.libraryName + " xxx");
+            if (!isLibraryOpen()) {
+                System.out.println("Sorry we are closed right now please try agin betwin" + openHour+" - " + closeHour);
+            } else {
+                while (true) {
+                    System.out.println("For see options write lib -h");
+                    System.out.println("For exit write exit");
+                    Scanner input = new Scanner(System.in);
+                    String command = input.nextLine();
+                    checkCommand(command);
+                }
+
+            }
+        }
+    }
+
--- Answers/40230112089/Library/src/Main.java
+++ Answers/40230112089/Library/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Library lib = new Library("NIT library", "7 - 16 ", "1383");
-
+        Library lib = new Library("NIT library", 7, 23 , "nit83");
+        lib.run();
     }
 }
