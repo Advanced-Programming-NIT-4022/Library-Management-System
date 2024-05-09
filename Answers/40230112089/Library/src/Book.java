9179c8ab5032c87c9b174b9f5b74206362023197
Zeinab <darya.abbasi1383@gmail.com>
2024-05-09 13:44:25 +0330

    book class completed

--- Answers/40230112089/Library/src/Book.java
+++ Answers/40230112089/Library/src/Book.java
@@ -1,2 +1,13 @@
-public class Book {
+public class Book  {
+    int bookId;
+    boolean isAvailable;
+    String bookAuthor;
+    String bookName;
+    public Book(String bookName,String bookAuthor,int bookId,boolean isAvailable){
+        this.isAvailable=isAvailable;
+        this.bookAuthor=bookAuthor;
+        this.bookId=bookId;
+        this.bookName=bookName;
+
+    }
 }
