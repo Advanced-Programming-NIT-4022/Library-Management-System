a7c142de99caf122455a22e30334898af68ba83c
AmirHosseinZg <zakeri863@gmail.com>
2024-05-09 18:16:25 +0330

    MyApp class created

---
+++ Answers/LMS_40230112063/src/main/java/ui/MyApp.java
@@ -0,0 +1,10 @@
+package ui;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+
+        userInterface.libraryLobby();
+    }
+}
