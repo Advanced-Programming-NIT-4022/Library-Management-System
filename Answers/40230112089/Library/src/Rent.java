90b65014b6871b39fd591ad6d5922fa8c559531f
Zeinab <darya.abbasi1383@gmail.com>
2024-05-09 14:13:01 +0330

    Rent class completed

--- Answers/40230112089/Library/src/Rent.java
+++ Answers/40230112089/Library/src/Rent.java
@@ -1,2 +1,14 @@
 public class Rent {
+    int rentId;
+    Book book;
+    User user;
+    Library libName;
+    String rentDate;
+    public Rent(int rentId , String rentDate , User user , Book book , Library libName){
+        this.book = book;
+        this.rentDate = rentDate;
+        this.rentId = rentId;
+        this.libName = libName;
+        this.user = user;
+    }
 }
