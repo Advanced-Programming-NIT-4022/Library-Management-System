2c825a25aff115b188c388b436d0d858b0a42c5d
Zeinab <darya.abbasi1383@gmail.com>
2024-05-09 22:49:44 +0330

    project compleated

--- Answers/40230112089/Library/src/Main.java
+++ Answers/40230112089/Library/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Library lib = new Library("NIT library", 7, 23 , "nit83");
+        Library lib = new Library("NIT library", 7, 18 , "nit83");
         lib.run();
     }
 }
