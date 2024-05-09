0e50f6f8c0191c5671c0fb1fdbe9f1136e36a943
Zeinab <darya.abbasi1383@gmail.com>
2024-05-09 14:04:34 +0330

    Rent class added

--- Answers/40230112089/Library/src/Library.java
+++ Answers/40230112089/Library/src/Library.java
@@ -1,2 +1,10 @@
+import java.util.*;
 public class Library {
+    String libraryName;
+    String libraryPassword;
+    String workingHours;
+    ArrayList<User> userArray = new ArrayList<>();
+    ArrayList<Book> bookArray = new ArrayList<>();
+    ArrayList<Library> libArray = new ArrayList<>();
+
 }
