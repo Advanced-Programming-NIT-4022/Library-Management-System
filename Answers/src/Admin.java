01fb444f030dd47ccf2cc7bdc68f9d51e9502339
Hossein <hossein.mofidimohajjerr@gmail.com>
2024-05-10 04:50:04 +0330

    change the regex in validation

--- Answers/src/Admin.java
+++ Answers/src/Admin.java
@@ -7,9 +7,9 @@ public class Admin extends User {
 
         // validate password
         if (!password.matches(".*\\d.*") && !password.matches(".*[a-z].*") &&
-                !password.matches(".*[A-Z].*"))
+                !password.matches(".*[A-Z].*") && !password.matches("[^(\"\\s)]*"))
             throw new IllegalArgumentException("Password should contain at least one digit," +
-                    "one capital and small letter.");
+                    "one capital and small letter\nand should not have \" character and white spaces.");
 
         this.password = password;
     }
