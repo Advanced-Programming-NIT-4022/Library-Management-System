f644c49dc21c5c14af5b42bbb630be3338f569f9
Amir <salehamir708@gmail.com>
2024-05-10 16:18:10 +0330

    Password is added to User class

--- Answers/40230112081/User.java
+++ Answers/40230112081/User.java
@@ -3,14 +3,16 @@ public class User {
     private String userID;
     private String phonenumber;
     private String role;
+    private String password;
 
     // constructor
-    public User(String userName, String userId, String phoneNumber, String role)
+    public User(String userName, String userId, String phoneNumber, String role, String _pass)
     {
         this.role = role;
         this.phonenumber = phoneNumber;
         this.user_name = userName;
         this.userID = userId;
+        this.password = _pass;
     }
 
     // getters
@@ -18,4 +20,5 @@ public class User {
     public String getUser_name() { return this.user_name; }
     public String getPhonenumber() { return this.phonenumber; }
     public String getRole() { return this.role; }
+    public String getPassword() { return this.password; }
 }
