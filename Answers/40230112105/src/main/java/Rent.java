6e1d25a1ebf923a75b25e852178134660e57c7e8
Nima <Nimafeli138384@gmail.com>
2024-05-10 11:47:58 +0330

    complete some parts of the menu-some parts of NormalUser and Rent-

--- Answers/40230112105/src/main/java/Rent.java
+++ Answers/40230112105/src/main/java/Rent.java
@@ -1,2 +1,23 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Rent {
+    public void rentBook(String id, String userId) {
+        ResultSet rs = Connect.getConnectExecute("SELECT availability FROM book WHERE bookid = '" +id+ "'");
+        try {
+            if (rs.next() || rs.getString("availability").equals("true")) {
+                ResultSet rs2 = Connect.getConnectExecute("SELECT id FROM student WHERE id = '" +userId+ "'");
+                if(rs2.next()){
+                    Connect.getConnect("INSERT INTO rent (bookid, id) VALUES ('" +id+ "','" + userId+ "')");
+                    System.out.println("Done!Here It's your book");
+                } else {
+                    System.out.println("There is no account with this ID");
+                }
+            } else {
+                System.out.println("The book isn't available or doesn't exist!");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
