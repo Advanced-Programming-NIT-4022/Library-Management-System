8f17f5a8458e4f62495f1ad153ae7dff11d38d13
Hossein <hossein.mofidimohajjerr@gmail.com>
2024-05-11 02:52:53 +0330

    Change order of commands

--- Answers/src/Manual.java
+++ Answers/src/Manual.java
@@ -8,34 +8,35 @@ public class Manual {
         // command log out
         System.out.println("lib logout : Log out from your account");
 
-        // command add
-        System.out.println("lib add book <name> <author> <subtitle[optional]> : Add a new book to the library.");
-
         // command hrs
         System.out.println("lib get hrs : Retrieve library operating hours.");
 
+        // command view book
+        System.out.println("lib get available books : View available books for rental.");
+
         // command rent from library
         System.out.println("lib rent <bookName> : Rent a book from the library.");
+
+        // command exit
+        System.out.println("lib exit : exit the program");
+
+        // command add book
+        System.out.println("lib add book <name> <author> <subtitle[optional]> : " +
+                "Add a new book to the library.");
 
         // command add member
         System.out.println("lib add member <name> <phoneNumber> <password[optional]>" +
                 " : Add a new member to the library (admin privilege required).");
 
-        // command rent for member
-        System.out.println("lib rent <bookName> <memberName> <memberID>" +
-                " : Rent a book for a specific member.");
-
-        // command view book
-        System.out.println("lib get available books : View available books for rental.");
-
         // command remove member
         System.out.println("lib remove member <memberID>" +
                 ": Remove a member from the library (admin privilege required).");
 
+        // command rent for member
+        System.out.println("lib rent <bookName> <memberName> <memberID>" +
+                " : Rent a book for a specific member.");
+
         // command return book
         System.out.println("lib return <bookName> : Return a rented book to the library.");
-
-        // command exit
-        System.out.println("lib exit : exit the program");
     }
 }
