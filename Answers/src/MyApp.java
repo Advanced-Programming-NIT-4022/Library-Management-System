0880efc11e2bc738557f6ee77ee2cf95a9714256
Hossein <hossein.mofidimohajjerr@gmail.com>
2024-05-10 14:15:43 +0330

    add command lib add book to CLI

--- Answers/src/MyApp.java
+++ Answers/src/MyApp.java
@@ -1,5 +1,6 @@
 // main class for handling the CLI
 
+import javax.naming.NoPermissionException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -84,7 +85,14 @@ public class  MyApp {
                 command = input.nextLine();
                 CLI(command.trim());
             } catch (IllegalArgumentException e) {
+                if (e.getMessage() == null)
+                    System.out.printf("command <%s> %s%n", command.trim(), "Not Found.");
+                else
+                    System.out.printf("%s%n", e.getMessage());
+            } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.printf("command <%s> %s%n", command.trim(), "Not Found.");
+            } catch (NoPermissionException e) {
+                System.out.printf("%s%n", e.getMessage());
             }
         }
     }
@@ -126,14 +134,81 @@ public class  MyApp {
         return true;
     }
 
-    public static void CLI(String command) throws IllegalArgumentException {
+    public static void CLI(String command) throws IllegalArgumentException, NoPermissionException {
         if (command.matches("lib\\sman"))
             Manual.print();
-        else if (command.matches("lib\\sadd\\sbook\\s\"[^\"]+\" \"")) {
+        else if (command.matches("lib\\sadd\\sbook\\s\"?[^\"]*\"?\\s\"?[^\"]*\"?\\s?(\"?[^\"]*\"?)?")) {
+            // if he didn't admin user yet throw exception
+            if (library.user instanceof Admin) {
+                String title, author;
 
+                // delete lib add book from command
+                command = command.substring(13);
+
+                // find the name of the book
+                if (command.startsWith("\"")) {
+                    command = command.substring(1);
+                    title = command.substring(command.indexOf("\""));
+
+                    // delete title of the book from command
+                    command = command.substring(command.indexOf("\""));
+                } else {
+                    title = command.substring(command.indexOf(" "));
+
+                    // delete title of the book from command
+                    command = command.substring(command.indexOf(" "));
+                }
+
+                // find the author of the book
+                if (command.startsWith("\"")) {
+                    command = command.substring(1);
+                    author = command.substring(command.indexOf("\""));
+
+                    // delete author of the book from command
+                    command = command.substring(command.indexOf("\""));
+                } else {
+                    author = command.substring(command.indexOf(" "));
+
+                    // delete author of the book from command
+                    command = command.substring(command.indexOf(" "));
+                }
+
+                // if command doesn't have description
+                if (command == null) {
+                    Book book = new Book(title, author);
+                    library.addBook(book);
+                    return;
+                }
+
+                String description;
+
+                // find the desciption of the book
+                if (command.startsWith("\"")) {
+                    command = command.substring(1);
+                    description = command.substring(command.indexOf("\""));
+
+                    // delete description of the book from command
+                    command = command.substring(command.indexOf("\""));
+                } else {
+                    description = command.substring(command.indexOf(" "));
+
+                    // delete description of the book from command
+                    command = command.substring(command.indexOf(" "));
+                }
+
+                // if command has too many argument
+                if (command != null)
+                    throw new IllegalArgumentException(
+                            "You should use \" character to split string with space");
+
+                Book book = new Book(title, author, description);
+                library.addBook(book);
+            } else
+                throw new NoPermissionException("You don't have permission to add books!");
         } else if (command.matches("lib return \\w*")) {
             // return a book
-        } else if (command.matches("lib\\sadd\\smember\\s\"?.+\"?\\s+?\\d+\\s([^(\"\\s)]*)?")) {
+        } else if (command.matches("lib\\sadd\\smember\\s\"?[^\"]*\"?\\s\\+?\\d+\\s?[^(\"\\s)]*")) {
+            // if he didn't admin user yet throw exception
             if (library.user instanceof Admin) {
                 String name, phoneNumber;
 
@@ -153,6 +228,11 @@ public class  MyApp {
                     }
                 } else {
                     String[] temp = command.split("\\s");
+
+                    if (temp.length > 6)
+                        throw new IllegalArgumentException(
+                                "You should use \" character to split string with space");
+
                     name = temp[3];
                     phoneNumber = temp[4];
 
@@ -164,14 +244,14 @@ public class  MyApp {
                         user = new NormalUser(name, phoneNumber);
                         library.addUser((NormalUser) user);
                     }
-                }
-            }
-            else
-                System.out.println("You don't have permission to add members!");
-        } else if (command.matches("li\\sexit")) {
-            System.out.println("Bye.");
+                } // end if (library.user instanceof Admin)
+            } else
+                throw new NoPermissionException("You don't have permission to add members!");
+        } // end lib add member
+        else if (command.matches("lib\\sexit")) {
+            System.out.print("Bye.");
             System.exit(0);
-        }
+        } // end lib exit
         else
             throw new IllegalArgumentException();
     }
