07ee5b7eb940c61d82a88fd54df64c55f4dde974
Niki.Malakpour <niki.malakpour@gmail.com>
2024-05-10 19:44:32 +0330

    Library Class started

---
+++ Answers/40230212079/Library.java
@@ -0,0 +1,18 @@
+public class Library {
+    String LibName , Libpassword ;
+    int LibOpenHour , LibCloseHour ;
+    public Library(String LibName, String Libpassword, int LibOpenHour, int LibCloseHour) {
+        this.LibName = LibName;
+        this.Libpassword = Libpassword ;
+        this.LibOpenHour = LibOpenHour;
+        this.LibCloseHour = LibCloseHour;
+    }
+    public void start( ){
+        System.out.println("welcome to the " + this.LibName + " library") ;
+        System.out.println("enter the options number of the following menu please");
+        System.out.println("1.Admin");
+        System.out.println("2.Normal User");
+        System.out.println("3.joind library");
+    }
+
+}
--- Answers/40230212079/MyApp.java
+++ Answers/40230212079/MyApp.java
@@ -1,2 +1,6 @@
 public class MyApp {
+    public static void main(String[] args){
+        Library obj = new Library("NIT","1234",8,20);
+        obj.start();
+    }
 }
