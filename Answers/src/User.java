b41e6a1e5e53e81b556d72adb14e641000bd6059
Hossein <hossein.mofidimohajjerr@gmail.com>
2024-05-15 04:06:03 +0330

    add user.toString method

--- Answers/src/User.java
+++ Answers/src/User.java
@@ -1,18 +1,17 @@
 // this class simulates the users
 // and it is superclass for NormalUser and Admin
-
 import java.sql.*;
 
-public class User {
+public class User implements Updatable {
     private String name;
-    private String uniqueID; // unique id automatically creates in database
+    private int uniqueID; // unique id automatically creates in database
     private String phoneNumber;
 
     // constructor
     public User(String name, String phoneNumber) throws IllegalArgumentException {
         // validate phoneNumber
-        String regexPhoneNumber = "(\\+989|09|00989)(0[0-5]|1[0-9]|2[0-3]|3[0-9]|9[0-6]-?\\d{3}-?\\d{4})|" +
-                "(991[0134]{5})|(99[09]{6})|(998{6})";
+        String regexPhoneNumber = "(\\+989|09|00989)((0[0-5]|1[0-9]|2[0-3]|3[0-9]|9[0-6])-?\\d{3}-?\\d{4})|" +
+                "(99-?1[0134]{2}-?[0134]{4})|(99-?[09]{3}-?[09]{4})";
         if (phoneNumber != null && !phoneNumber.matches(regexPhoneNumber))
             throw new IllegalArgumentException("Invalid phone number!");
 
@@ -20,8 +19,8 @@ public class User {
         this.phoneNumber = phoneNumber;
     }
 
-    public void uniqueIDUpdate() {
-        final String SQL_COMMAND = "SELECT UserID FROM Users WHERE PhoneNumber = ?";
+    public void update() {
+        final String SQL_COMMAND = "SELECT UserID FROM users WHERE PhoneNumber = ?";
 
         try (Connection connection = DriverManager.getConnection(MyApp.DB_URL,
                 MyApp.DB_USERNAME, MyApp.DB_PASSWORD);
@@ -30,7 +29,8 @@ public class User {
             selectID.setString(1, this.phoneNumber);
 
             ResultSet resultSet = selectID.executeQuery();
-            this.uniqueID = resultSet.getString(1);
+            if (resultSet.next())
+                this.uniqueID = resultSet.getInt("UserID");
         } catch (SQLException sqlException) {
             sqlException.printStackTrace();
             System.err.print("Connection to database failed! Terminating...");
@@ -46,7 +46,13 @@ public class User {
         return phoneNumber;
     }
 
-    public String getUniqueID() {
+    public int getUniqueID() {
         return uniqueID;
+    }
+
+    @Override
+    // to string method of admins
+    public String toString() {
+        return String.format("You are "+ this.getName() + " with id = " + this.getUniqueID());
     }
 }
