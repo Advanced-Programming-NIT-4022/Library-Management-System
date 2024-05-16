50519f3f597cb169c150dade4b61e1ae58828fae
HJavid010 <HJavid0010@gmail.com>
2024-05-16 15:39:44 +0330

    added OptionSelector class to check for argument and run the desired function.

---
+++ Answers/40230112037/Library-Management-System/src/main/java/org/LMS/OptionSelector.java
@@ -0,0 +1,30 @@
+package org.LMS;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class OptionSelector {
+    ArrayList<Option> options = new ArrayList<>();
+
+    void add(String optionName, Func function) {
+        options.add(new Option(optionName, function));
+    }
+
+    void add(ArrayList<Option> options) {
+        this.options.addAll(options);
+    }
+
+    void select(String[] arguments) {
+        if (arguments.length == 0) {
+            PrintError.fewArguments();
+            return;
+        }
+        for (Option option : this.options) {
+            if (option.option.equals(arguments[0])) {
+                option.func(Arrays.copyOfRange(arguments, 1, arguments.length));
+                return;
+            }
+        }
+        System.out.printf("Unknown option %s.\n", arguments[0]);
+    }
+}
