3e4737244ce05c9bb537ec757bd1ab23681c1e3e
Niki.Malakpour <niki.malakpour@gmail.com>
2024-05-16 22:48:06 +0330

    NormalUser

--- Answers/40230212079/NormalUser.java
+++ Answers/40230212079/NormalUser.java
@@ -1,2 +1,15 @@
-public class NormalUser {
+import java.util.* ;
+public class NormalUser extends User {
+    private String userdate ;
+    Scanner input1 = new Scanner (System.in);
+    public NormalUser(String name , int ID , String phonnumber){
+        super(name, ID, phonnumber);
+        System.out.println("please enter the date");
+        userdate = input1.nextLine();
+
+    }
+    public String getUserDate()
+    {
+        return userdate;
+    }
 }
