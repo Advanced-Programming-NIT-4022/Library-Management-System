fc1a93caeac73b1ab072b9ebaf5847bb0b3e1c03
seyedkosarmousavi <seyedkosarmousavi2005@gmail.com>
2024-05-16 22:21:16 +0330

    add Admin class

---
+++ Answers/java project/Admin.java
@@ -0,0 +1,12 @@
+public class Admin extends User {
+    private String password;
+
+    public Admin(String name, String phoneNumber, String password) {
+        super(name, phoneNumber);
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
