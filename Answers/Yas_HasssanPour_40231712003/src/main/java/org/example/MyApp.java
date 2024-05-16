c8a90de86dd315a060fe8d835c8f258762d727bb
Yas Hassan Pour <yusshp1989@gmail.com>
2024-05-16 23:08:21 +0330

    edited

--- Answers/Yas_HasssanPour_40231712003/src/main/java/org/example/MyApp.java
+++ Answers/Yas_HasssanPour_40231712003/src/main/java/org/example/MyApp.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class MyApp {
         public static void main(String[] args) {
-            // Initialize some sample data
+
             Library library = new Library("My Library", 100, 9_5);
 
             Book book1 = new Book("Book2", 82734,"author1", "Description1");
@@ -15,7 +15,7 @@ public class MyApp {
             NormalUser user1 = new NormalUser("User1", 1234567890,"09112143646", new Date());
             library.addUser(user1);
 
-            // Example commands
+
             library.rentBook(book1, user1, 1, new Date());
             library.returnBook(book1);
             library.removeUser(user1);
