d778260f593669070d7a607e31609536167f8b82
Niki.Malakpour <niki.malakpour@gmail.com>
2024-05-16 22:56:36 +0330

    Admin

--- Answers/40230212079/Admin.java
+++ Answers/40230212079/Admin.java
@@ -1,2 +1,17 @@
-public class Admin {
+public class Admin extends User {
+    private String Password;
+    public Admin(String name , int ID , String phonnumber , String Password)
+    {
+        super(name, ID, phonnumber);
+        this.setPassword(Password);
+    }
+    public void setPassword(String Password)
+    {
+        this.Password=Password;
+    }
+    public String getPassword()
+    {
+        return Password;
+    }
+
 }
