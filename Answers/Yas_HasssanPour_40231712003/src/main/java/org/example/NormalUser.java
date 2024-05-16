c22fbbdd18effc1b737edc263636212154b18616
Yas Hassan Pour <yusshp1989@gmail.com>
2024-05-16 10:21:59 +0330

    Normal User class

---
+++ Answers/Yas_HasssanPour_40231712003/src/main/java/org/example/NormalUser.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Date;
+
+public class NormalUser extends User{
+    private Date registrationDate;
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public NormalUser(String name, int ID, int phoneNum, Date registrationDate)
+    {
+        super(name,ID,phoneNum);
+        this.registrationDate= registrationDate;
+    }
+}
