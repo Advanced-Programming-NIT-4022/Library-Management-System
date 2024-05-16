9b3b11b8f6adb96139d6bda237e1bd32f57247ba
Yas Hassan Pour <yusshp1989@gmail.com>
2024-05-16 10:19:29 +0330

    Book class

---
+++ Answers/Yas_HasssanPour_40231712003/src/main/java/org/example/Book.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class Book {
+    private int Unique_bookID;
+    private String title;
+    private String author;
+    private boolean Availability_status;
+    String Description;
+
+    public int getUnique_bookID() {
+        return Unique_bookID;
+    }
+
+    public void setUnique_bookID(int unique_bookID) {
+        Unique_bookID = unique_bookID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isAvailability_status() {
+        return Availability_status;
+    }
+
+    public void setAvailability_status(boolean availability_status) {
+        Availability_status = availability_status;
+    }
+
+
+    public Book (int bookID, String title, String author, String description) {
+        Unique_bookID = bookID;
+        this.title = title;
+        this.author = author;
+        this.Availability_status = true;
+        this.Description= description;
+    }
+}
+
