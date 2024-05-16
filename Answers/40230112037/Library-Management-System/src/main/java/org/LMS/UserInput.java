6c31cae97a9668602ca045dc31f425d08a72d725
HJavid010 <HJavid0010@gmail.com>
2024-05-16 16:25:08 +0330

    added a regex to split user input arguments.

--- Answers/40230112037/Library-Management-System/src/main/java/org/LMS/UserInput.java
+++ Answers/40230112037/Library-Management-System/src/main/java/org/LMS/UserInput.java
@@ -5,6 +5,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserInput {
+    public static String[] argumentsToArray(String input) {
+        Pattern pattern = Pattern.compile("(?>(?<bare>[^\\\"\\s](?:\\\\\\\\|\\\\ |[^ ])*?)(?> |$)|(?>\\\"(?<quo>(?:\\\\\\\\|\\\\\\\"|[^\\\"])*?)\\\"))");
+        Matcher matcher = pattern.matcher(input);
+        ArrayList<String> result = new ArrayList<String>();
+        while (matcher.find()) {
+            if (matcher.group("bare") != null) {
+                result.add(matcher.group("bare").replaceAll("\\\\\\\\", "\\\\").replaceAll("\\\\ ", " "));
+            } else {
+                result.add(matcher.group("quo").replaceAll("\\\\\\\\", "\\\\").replaceAll("\\\\\"", "\""));
+            }
+        }
+        return result.toArray(new String[0]);
+    }
 
     public static String sqlLikeRegex(String searchString) {
         return searchString.replace("!", "\\!").replace("%", "\\%").replace("_", "\\_").replace("[", "\\[");
