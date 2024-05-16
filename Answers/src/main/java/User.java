e1b9fda0e20ae1170fd9f9fda7fbba2bbe4e1ba7
yekta <yekta.rjd@gmail.com>
2024-05-16 23:36:12 +0330

    class user added

--- Answers/src/main/java/User.java
+++ Answers/src/main/java/User.java
@@ -1,11 +1,31 @@
 public class User {
-    String Name;
-    int Unique_ID;
-    int Phone_number;
+    private String Name;
+    private long Unique_ID;
+    private String Phone_number;
 
-    public User(String Name , int Unique_ID , int Phone_number){
+    public User(String Name , long Unique_ID , String Phone_number){
         this.Name = Name;
         this.Unique_ID = Unique_ID;
         this.Phone_number = Phone_number;
     }
+
+    public String getName() {
+        return Name;
+    }
+   /* public void setName(String name) {
+        Name = name;
+    }*/
+
+    public long getUnique_ID() {
+        return Unique_ID;
+    }
+    public void setUnique_ID(int unique_ID) {
+        Unique_ID = unique_ID;
+    }
+
+    public String getPhone_number() {
+        return Phone_number;
+    }
 }
+
+
