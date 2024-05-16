dca81f0d8a06dbf3b2f63fa3c7dab42b021693b8
Niki.Malakpour <niki.malakpour@gmail.com>
2024-05-16 22:32:16 +0330

    class user

--- Answers/40230212079/User.java
+++ Answers/40230212079/User.java
@@ -1,2 +1,34 @@
 public class User {
+    private String name;
+    private int ID;
+    private String phonnumber;
+
+    public User(String name, int ID, String phonnumber) {
+        this.setName(name);
+        this.ID = ID;
+        this.setName(phonnumber);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getphonNumber() {
+        return phonnumber;
+    }
+
+    public void setphonNumber(String number) {
+        this.phonnumber = phonnumber;
+    }
+
+    public int getID() {
+        return ID;
+    }
+    public void setID(int ID){
+        this.ID=ID;
+    }
 }
