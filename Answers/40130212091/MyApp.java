5a92c629095a31b1487140933b5c5577ae9a4566
muhammad <maliniyakani@gmail.com>
2024-05-16 19:09:39 +0430

    updated

--- Answers/40130212091/MyApp.java
+++ Answers/40130212091/MyApp.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class MyApp {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         Library library = new Library();
         System.out.println("Hello welcome to " + library.getLibraryName());
         System.out.println("capacity : " + library.getCapacity());
@@ -11,60 +11,54 @@ public class MyApp {
 
         Scanner command = new Scanner(System.in);
         String input;
-        while(true) {
+        while (true) {
             System.out.println("~~~  ");
             input = command.nextLine();
-            if (input.equalsIgnoreCase("exit")){
+            if (input.equalsIgnoreCase("exit")) {
                 break;
             }
-            commandLine(input.split(" "));
+            commandLine(input.split(" "), library);
         }
         command.close();
     }
 
-    public static void commandLine(String[] order){
-        Library library = new Library();
+    public static void commandLine(String[] order, Library library) {
         try {
-            if (order[1].equals("lib")) {
-                if (order[2].equals("add")) {
-                    if (order[3].equals("book")) {
-                        library.addBook(order[4], order[4], order[5]);
-                    } else if (order[3].equals("member")) {
-                        library.addMember(order[4], order[5]);
-                    }
-                } else if (order[2].equals("rent")) {
-                    library.rentBook(order[4], order[5]);
-                } else if (order[2].equals("get")) {
-                    if (order[3].equals("hrs")) {
-                        System.out.println(library.getHours());
-                    } else if (order[3].equals("available")) {
-                        if (order[4].equals("books")) {
-                            library.availableBooks();
-                        }
-                    }
-                } else if (order[2] == "remove") {
-                    if (order[3] == "member") {
-                        library.removeMember(order[4]);
-                    }
-                } else if (order[2].equals("return")) {
-                    library.returnBook(order[4]);
-                }
-                else if (order[2].equals("help")){
-                    System.out.println("lib add book <name> <author> <subtitle>: Add a new book to the library.\n" +
-                            "lib get hrs: Retrieve library operating hours.\n" +
-                            "lib rent <bookName>: Rent a book from the library.\n" +
-                            "lib add member <studentID> <password>: Add a new member to the library (admin privilege required).\n" +
-                            "lib rent <bookName> <memberName> <memberID>: Rent a book for a specific member.\n" +
-                            "lib get available books: View available books for rental.\n" +
-                            "lib remove member <memberID>: Remove a member from the library (admin privilege required).\n" +
-                            "lib return <bookName>: Return a rented book to the library.\n"+
-                            "lib add admin <AdminName> <PhoneNumber>: Add a new admin to the library\n" +
-                            ">>> ");
-                }
-            }
+            if (order[1].equals("lib") && order[2].equals("add") && order[3].equals("book")) {
+                library.addBook(order[4], order[5], order[6]);
+            } else if (order[1].equals("lib") && order[2].equals("add") && order[3].equals("member")) {
+                library.addMember(order[4], order[5]);
+            } else if (order[1].equals("lib") && order[2].equals("rent")) {
+                library.rentBook(order[4], order[5]);
+            } else if (order[1].equals("lib") && order[2].equals("get") && order[3].equals("hrs")) {
+                System.out.println(library.getHours());
+            } else if (order[1].equals("lib") && order[2].equals("get") && order[3].equals("books")) {
+                library.availableBooks();
+            } else if (order[1].equals("lib") && order[2].equals("remove") && order[3].equals("member")) {
+                library.removeMember(order[4]);
 
-        } catch (Exception e) {
-            throw new RuntimeException("enter a correct command");
-        }
+            } else if (order[1].equals("lib") && order[2].equals("return")) {
+                library.returnBook(order[4]);
+            } else if (order[1].equals("lib") && order[2].equals("help")) {
+                System.out.println("lib add book <name> <author> <subtitle>: Add a new book to the library.\n" +
+                        "lib get hrs: Retrieve library operating hours.\n" +
+                        "lib rent <bookName>: Rent a book from the library.\n" +
+                        "lib add member <studentID> <password>: Add a new member to the library (admin privilege required).\n" +
+                        "lib rent <bookName> <memberName> <memberID>: Rent a book for a specific member.\n" +
+                        "lib get available books: View available books for rental.\n" +
+                        "lib remove member <memberID>: Remove a member from the library (admin privilege required).\n" +
+                        "lib return <bookName>: Return a rented book to the library.\n" +
+                        "lib add admin <AdminName> <PhoneNumber>: Add a new admin to the library\n" +
+                        ">>> ");
+            }
+            else{
+                throw new Exception();
+            }
+        } catch(
+    Exception e)
+
+    {
+        throw new RuntimeException("enter a correct command");
     }
+}
 }
